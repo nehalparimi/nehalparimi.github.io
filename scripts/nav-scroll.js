78c72532370b496441180fda586784b17242f6fa
Nehal Parimi <parimi.nehal@gmail.com>
2020-06-11 12:24:18 -0700

    Change nav bar

---
+++ scripts/nav-scroll.js
@@ -0,0 +1,21 @@
+(function navScroll() {
+    let nav = document.getElementById('xyz');
+    let banner = document.getElementById('banner');
+    let bannerUnderline = document.getElementById('banner-underline')
+    console.log(banner);
+
+    document.addEventListener('scroll', function(e) {
+        console.log(document.documentElement.scrollTop);
+        
+        if (document.documentElement.scrollTop > 65) {
+            nav.classList.add('nav-container');
+            banner.classList.add('banner-str');
+            bannerUnderline.classList.add('banner-underline-str');
+        }
+        else {
+            nav.classList.remove('nav-container');
+            banner.classList.remove('banner-str');
+            bannerUnderline.classList.remove('banner-underline-str');
+        }
+    })
+})();
