ef598b8000096f1a6a48e0625e99b00ae492901e
Nehal Parimi <parimi.nehal@gmail.com>
2020-06-11 15:23:30 -0700

    Add some transforms

--- scripts/nav-scroll.js
+++ scripts/nav-scroll.js
@@ -3,16 +3,31 @@
     let banner = document.querySelector('.banner');
     let bannerUnderline = document.querySelector('.banner-underline');
 
+    let navLinks = Array.from(document.getElementsByClassName('nav-link'));
+
     document.addEventListener('scroll', function(e) {
         // console.log(nav.clientHeight);
         // check the client height part later
         if (document.documentElement.scrollTop > nav.clientHeight) {
             banner.classList.add('banner-scroll');
             bannerUnderline.classList.add('banner-underline-scroll');
+
+            navLinks.forEach(link => link.classList.add('nav-link-scroll'));
         }
         else {
             banner.classList.remove('banner-scroll');
             bannerUnderline.classList.remove('banner-underline-scroll');
+
+            navLinks.forEach(link => link.classList.remove('nav-link-scroll'))
         }
     })
+
+    // Just for fun, can remove if not needed:
+        let bannerContainer = banner.parentElement;
+        bannerContainer.addEventListener('mouseover', function () {
+            banner.classList.add('boo');
+        });
+        bannerContainer.addEventListener('mouseleave', function () {
+            banner.classList.remove('boo');
+        })
 })();
